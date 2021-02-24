e7afdfe48049118025899665399ae36c33168249
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-24 21:05:49 +0200

    Initial Commit

---
+++ AllAboutListsInSwiftUI/AllAboutListsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutListsInSwiftUIApp.swift
+//  AllAboutListsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 24.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutListsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
